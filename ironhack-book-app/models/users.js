107fc90f163edf1abe6526453d0e3e6f79ed1f1f
Boyuan Shi <sbyshjd@hotmail.com>
2020-03-31 21:27:56 +0200

    Create the models

---
+++ ironhack-book-app/models/users.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    username:{type:String, required:true, unique:true},
+    password:{type:String},
+    favorites:[{type:String}],
+    wishList:[{type:String}],
+    reviews:[{ type: Schema.Types.ObjectId, ref: 'Review' }],
+    timestamps:true,
+});
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
