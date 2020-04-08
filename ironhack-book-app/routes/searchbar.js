4fc18868e54420218014b45d6fbe34f1517cbfe5
Michiel Snoek <michiel.snoek@gmail.com>
2020-04-08 10:55:18 +0200

    added searchresult functionality

--- ironhack-book-app/routes/searchbar.js
+++ ironhack-book-app/routes/searchbar.js
@@ -1,28 +1,33 @@
 const express       = require('express');
 const searchRouter  = express.Router();
 const axios         = require('axios');
-
+const User          = require('../models/users');
 const checkroles    = require('../auth/checkroles')
-let books = null
 
 searchRouter.post('/search-results',(req, res, next) => {
   console.log(req.body.searchinput)
   const searchInput = req.body.searchinput
+  const isLogged = req.isAuthenticated()
   axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}&key=AIzaSyAMNHv1Hf_DoGzNa4RSTRzDJjM2QEE6uvs`)
   .then(response => {
-    console.log(response.data.items[0].volumeInfo)
-    books = response.data.items
-    res.redirect('/search-results')
+    //console.log(response.data.items[0].volumeInfo)
+    const books = response.data.items
+    books.map(book => book.islogged = isLogged)
+    res.render('searchresults', {books})
     })
-  .catch(error => console.log(error))
+  .catch(e => console.log(e))
 })
 
 searchRouter.get('/search-results',(req, res, next) => {
-  books.map(book => book.islogged = req.isAuthenticated())
-  res.render('searchresults', {books}) 
+  res.render('searchresults') 
 })
 
-
+searchRouter.get('/search-results/:id',(req, res, next) => {
+const id = req.params.id
+ User.update({_id: req.user._id},{$push: {favorites: id}})
+  .then(() => res.redirect('/search-results'))
+  .catch(e => console.log(e))
+})
 
 
 
